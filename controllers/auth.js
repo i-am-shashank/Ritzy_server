d29efbe8079789b67905bc05b45f7aa5aa92f0ac
this-is-shashank-mishra <geekyholic2275@gmail.com>
2021-07-30 18:37:22 +0530

    config: vercel

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const { createJWT } = require("../utils/auth");
 
 const emailRegexp =
